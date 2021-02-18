463a2a43b5382a3a6fa0d7086039599fcfdcae7f
i'mfebri <iamfebriadji@gmail.com>
2021-02-18 17:08:23 +0700

    update models animelists

--- models/animelists.js
+++ models/animelists.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const animeListsSchema = new Schema({
+  title: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  author: String,
+  rating: Number,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  synopsis: String
+})
+
+module.exports = mongoose.model('animelist', animeListsSchema)
